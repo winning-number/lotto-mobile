cebf4882c0471ea1375748c4ca21822d529c707a
Yannick MOHL-CLAUZADE <mohl.clauzade@gmail.com>
2022-08-15 20:18:42 +0200

    build android

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,5 @@
+package io.lotogagnant.starter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'loto-gagnant',
   appName: 'winning-number',
   webDir: 'dist',
   bundledWebRuntime: false
